59b7e448c30e1e96537c57c00932d76e88234825
Stu W <stubillwhite@gmail.com>
2019-09-25 13:15:43 +0100

    WIP while moving laptop

--- src/FindingSuccessInHaskell/Chapter01.hs
+++ src/FindingSuccessInHaskell/Chapter01.hs
@@ -1,5 +1,6 @@
 module FindingSuccessInHaskell.Chapter01
-  ( absVal
+  ( absVal,
+    validateUsernamePassword
   ) where 
 
 import Data.List
@@ -8,3 +9,11 @@ absVal :: (Num a, Ord a) => a -> a
 absVal x = case (x < 0) of
                  True -> (negate x)
                  False -> x
+
+validateUsernamePassword :: String -> String -> String
+validateUsernamePassword username password =
+  if null username
+    then (if null password
+        then "Empty username and password"
+        else "Empty username")
+    else (if null password                                     then "Empty password" else "Okay")
--- test/FindingSuccessInHaskell/Chapter01Spec.hs
+++ test/FindingSuccessInHaskell/Chapter01Spec.hs
@@ -3,7 +3,7 @@ module FindingSuccessInHaskell.Chapter01Spec (main, spec) where
 import Test.Hspec
 import Test.QuickCheck
 
-import FindingSuccessInHaskell.Chapter01 (absVal)
+import FindingSuccessInHaskell.Chapter01 (absVal, validateUsernamePassword)
 
 main :: IO ()
 main = hspec spec
@@ -14,5 +14,11 @@ spec = do
     it "should return the absolute value of positive and negative numbers" $ do
       absVal 10 `shouldBe` 10
       absVal (-10) `shouldBe` 10
-      
+  describe "validateUsernamePassword" $ do
+    it "should only validate if both username and password are supplied" $ do
+      (validateUsernamePassword []         [])         `shouldBe` "Empty username and password"
+      (validateUsernamePassword "username" [])         `shouldBe` "Empty password"
+      (validateUsernamePassword []         "password") `shouldBe` "Empty username"
+      (validateUsernamePassword "username" "password") `shouldBe` "Okay"
+
 
